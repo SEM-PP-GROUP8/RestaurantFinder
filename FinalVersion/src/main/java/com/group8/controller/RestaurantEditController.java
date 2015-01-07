28e0f9abc4602dc00503219d00fed9738fcddcca
Sergiu Ghita <sergiu.ghita@gmail.com>
2015-01-07 17:02:29 +0200

    Added RestaurantEditController and EditRestaurt views

---
+++ FinalVersion/src/main/java/com/group8/controller/RestaurantEditController.java
@@ -0,0 +1,39 @@
+package com.group8.controller;
+
+import com.group8.model.Restaurant;
+
+public class RestaurantEditController {
+
+    public void updateRestaurant() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void deleteRestaurant() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void createTemporaryRestaurant() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void saveNewRestaurant() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void removeTemporaryRestaurant() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void updateRestaurantList() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void loadDetailsAndSchedule(Restaurant r) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void closeView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
